2ac8191996b98f374835713baac92b06d821785c
clevo <fernando.chitarrini@gmail.com>
2019-06-09 21:11:07 +0100

    feat :Change welcome message in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -21,7 +21,7 @@ def get_filters():
         # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
         city = input(
-            "Welcome to the US Bike Share Mariobros! \n Which city would you like to explore, Washington, New York City or Chicago?\n").lower()
+            "Welcome to the US BikeShareFernando! \n Which city would you like to explore, Washington, New York City or Chicago?\n").lower()
         if city not in ['chicago', 'new york city', 'washington']:
             print("Sorry, I do not understand your input. Please input either chicago, new york city, or Washington.")
             continue
